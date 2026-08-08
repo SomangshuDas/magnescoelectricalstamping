950c7d3352a213437a2d775f69e3666d978bd133
SomangshuDas <somangshudas.dev@gmail.com>
2026-08-08 21:01:37 +0530

    Delete dashboard/

--- dashboard/js/config.js
+++
@@ -1,4 +0,0 @@
-export const CONFIG = {
-    machineDataEndpoint: "https://script.google.com/macros/s/AKfycbzFdbNrjWz5KrUvt4Hu-8I1iEmYvQc2VUkrng75Ol-Qwg4HzMfsADvRujA4YyI8prSV/exec", 	// Change according to your Google Apps Script "MAGNESCO Production Management System" Web App URL
-	dashboardEndpoint: "https://script.google.com/macros/s/AKfycbzElTn9KeVHCYYDKOuZSZ6gOK0Cymyy92nXXQN5J6MHm7PyehMg6D3B9FEVmExDl0S8Fw/exec" // Change according to your Google Apps Script "MAGNESCO Production Management System Dashboard" Web App URL
-};
--- dashboard/js/installer.js
+++
@@ -1,3 +0,0 @@
-window.addEventListener("load", function () {
-    window.location.replace("../assets/files/MAGNESCO_Production_Management_System.apk");
-});
